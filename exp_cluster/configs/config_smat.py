e3e0ca129a6df215f30b4fc1c7f88618072a3b00
adateng07 <adateng07@gmail.com>
2022-05-12 22:40:05 -0400

    Modified exp.

--- exp_cluster/configs/config_smat.py
+++ exp_cluster/configs/config_smat.py
@@ -9,13 +9,13 @@ def get_config():
   config.job_id = config_dict.placeholder(int)
   #
   config.iter_list = [2]
-  h_params = [0., 1.]
+  h_params = [0.3, 0.4, 0.5, 0.6]
   t_params = [0.1, 0.3, 0.7]
   # h_and_t = list(itertools.product(h_params, t_params))
   config.h_t_iter = list(itertools.product(h_params, t_params, config.iter_list))
   config.slice_idx = 6000
 
-  config.data_dir = "/Volumes/GoogleDrive/My Drive/Projects/ML_toric_code/Data_cluster/data/5978142/ensemble/7231166/"
+  config.data_dir = "/n/home11/yteng/experiments/optimization/data/5978142/ensemble/7592828/"
   # config.output_dir = os.path.join(config.data_dir, "estimates/")
   config.filenames_load = [f"samples_(6, 3)_hz{h}_T{T}_iter{i}.p" for (h, T, i) in config.h_t_iter]
   config.filenames_save = f"S_mat.nc"
