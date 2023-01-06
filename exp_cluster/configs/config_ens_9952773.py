4075af220a2d01c2d2cd809c008bc37c5d7eb44b
teng10 <adateng07@gmail.com>
2023-01-06 12:28:25 -0500

    Modified config files for experiments.

---
+++ exp_cluster/configs/config_ens_9952773.py
@@ -0,0 +1,18 @@
+import ml_collections
+import itertools
+
+def get_config():
+  config = ml_collections.ConfigDict()
+  config.iter = 2
+  config.field2 = 'tom'
+  config.burn_E_factor = 500
+  config.num_samples_E = 200
+  config.len_chain_E = 30
+  config.len_chain= 100
+  config.tuple = (1, 2, 3)
+  h_params = [0., 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 1.]
+  t_params = [0.1, 0.3, 0.7]
+  config.h_and_t = list(itertools.product(h_params, t_params))
+
+  return config
+
